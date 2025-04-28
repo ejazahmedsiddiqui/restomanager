d0188273ffa6c1443d654d4a4e6edc1725cb450b
Ejaz Ahmed <eahemdsiddiqui@gmail.com>
2025-04-28 18:07:13 +0530

    Add DishService interface

--- RestoManager/src/main/java/com/example/myorganisation/RestoManager/service/DishService.java
+++ RestoManager/src/main/java/com/example/myorganisation/RestoManager/service/DishService.java
@@ -1,4 +1,14 @@
 package com.example.myorganisation.RestoManager.service;
 
-public class DishService {
+import com.example.myorganisation.RestoManager.dto.DishInputDTO;
+import com.example.myorganisation.RestoManager.dto.DishOutputDTO;
+
+import java.util.List;
+
+public interface DishService {
+
+    DishOutputDTO createDish(DishInputDTO dishInputDTO);
+    DishOutputDTO getDish(Long id);
+    List<DishOutputDTO> getAllDish();
+
 }
